bd51c889b38d1081ce55f4729627bac6dcfbf162
drozdzu88@gmail.com <drozdzu88@gmail.com>
2023-05-20 10:54:31 +0000

    circuits.csv ingestion

---
+++ Formula1/ingestion/1.ingest_circuits_file.py
@@ -0,0 +1,55 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ###Ingest circuits.csv file
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 1-  Read the CSV file using the spark dataframe reader
+
+# COMMAND ----------
+
+display(dbutils.fs.mounts())
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls /mnt/lukaszdrozdformula1/raw
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType
+
+# COMMAND ----------
+
+circuits_schema = StructType(fields=[StructField("circuitId", IntegerType(), False),
+                                     StructField("circuitRef", StringType(), True),
+                                     StructField("name", StringType(), True),
+                                     StructField("location", StringType(), True),
+                                     StructField("country", StringType(), True),
+                                     StructField("lat", DoubleType(), True),
+                                     StructField("lng", DoubleType(), True),
+                                     StructField("alt", IntegerType(), True),
+                                     StructField("url", StringType(), True),
+                                     
+
+])
+
+# COMMAND ----------
+
+circuits_df = spark.read \
+.option("header", True) \
+.schema(circuits_schema) \
+.csv("dbfs:/mnt/lukaszdrozdformula1/raw/circuits.csv")
+
+# COMMAND ----------
+
+display(circuits_df)
+
+# COMMAND ----------
+
+circuits_df.printSchema()
+
+# COMMAND ----------
+
+circuits_df.describe().show()
